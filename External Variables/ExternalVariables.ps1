0c1f0d5bf6c0cd79980d4425e607f9ed5f8b9f1e
Øyvind Pettersen <oyvind_pettersen@outlook.com>
2022-11-28 15:36:42 +0100

    Created 'External Variables'

---
+++ External Variables/ExternalVariables.ps1
@@ -0,0 +1,3 @@
+#Declaration of External variables
+$External_Variable1 = ‘Sapien’
+$External_Variable2 = ‘Technologies’
---
+++ External Variables/ExternalVariables_Script.ps1
@@ -0,0 +1,4 @@
+# Here we dot source the External variables PowerShell File
+. "/Users/oyvindpettersen/Jottacloud/Coding/Powershell/_PS-Notater/External Variables/ExternalVariables.ps1"
+ 
+Write-Host $External_Variable1 $External_Variable2
